9764bc6681fa59f2314c4e2ccda5a63e1ae95813
Harshad Chavan <meharshadchavan@gmail.com>
2026-03-16 20:02:30 +0530

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo by harshad',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
